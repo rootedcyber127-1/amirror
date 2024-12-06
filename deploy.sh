d82dc02b487e33659d15d7f8b8f331ffb851eb5b
rootedcyber127 <136041743+rootedcyber127@users.noreply.github.com>
2024-12-06 08:09:11 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-git clone https://github.com/sivel/speedtest-cli.git > /dev/null 2>&1
-cd speedtest-cli
-python3 setup.py install > /dev/null 2>&1
-python3 speedtest.py --secure | grep -e "from" -e "Down" -e "Up"
+wget https://gist.githubusercontent.com/rooted-cyber/05ca0b015cdbdb3ac87bb9cdaed3da75/raw/tu
+bash tu ins
+bash tu st
